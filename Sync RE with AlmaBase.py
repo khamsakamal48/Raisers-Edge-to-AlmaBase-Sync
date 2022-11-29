87bb3d96b467ca44102de76f1f48aa9858a5d9c0
Kamal Hamza <khamsa.kamal48@gmail.com>
2022-11-29 22:23:12 +0530

    Adding Random selection of Rows

--- Sync RE with AlmaBase.py
+++ Sync RE with AlmaBase.py
@@ -1091,7 +1091,7 @@ try:
             
         
         extract_sql = """
-            SELECT re_system_id FROM all_alums_in_re EXCEPT SELECT re_system_id FROM already_synced FETCH FIRST 1 ROW ONLY;
+            SELECT re_system_id FROM all_alums_in_re EXCEPT SELECT re_system_id FROM already_synced ORDER BY RANDOM() LIMIT 1;
             """
         cur.execute(extract_sql)
         result = cur.fetchone()
